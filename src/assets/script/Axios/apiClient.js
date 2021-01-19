dbe48b0c571b0798c6246cc7143f95436f132799
Gura <ogurahiroki1999@yahoo.co.jp>
2021-01-19 16:27:29 +0900

    envの追加

--- src/assets/script/Axios/apiClient.js
+++ src/assets/script/Axios/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const apiClient = axios.create({
     // baseURL: "127.0.0.1:57752",
-    baseURL: "http://localhost:8000",
+    // baseURL: "http://localhost:8000",
+    baseURL: process.env.REACT_APP_DEV_API_URL,
     withCredentials: true,
     headers: {
       "Content-Type": "application/json",
